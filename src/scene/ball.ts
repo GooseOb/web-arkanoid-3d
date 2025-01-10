7e55d11971cdcbabfe1d1ed6ce5a28615839ea46
GooseOb <gooose.ob@gmail.com>
2025-01-10 11:42:41 +0100

    change ball material

--- src/scene/ball.ts
+++ src/scene/ball.ts
@@ -2,8 +2,11 @@ import * as THREE from "three";
 import { BALL_RADIUS } from "../constants";
 
 const ballGeometry = new THREE.SphereGeometry(BALL_RADIUS, 32, 32);
-const ballMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const ball = new THREE.PointLight(0xff6666, 2, 800);
+const ballMaterial = new THREE.MeshStandardMaterial({
+  color: 0xffff00,
+  emissive: new THREE.Color(0xff0000),
+});
+const ball = new THREE.PointLight(0xff0000, 2, 800);
 ball.add(new THREE.Mesh(ballGeometry, ballMaterial));
 
 export { ball };
